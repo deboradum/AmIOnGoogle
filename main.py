58351dba7d79d4b0f754cb31b1fcffff35115eeb
Pepijn <pvwpvw@protonmail.com>
2023-04-27 13:50:55 +0200

    Downloading images from the internet now working

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ import argparse
 from os import listdir
 import os.path
 import itertools
+from PIL import Image
+import requests
+from io import BytesIO
 
 
 ACCEPTED_FORMATS = ['png', 'PNG', 'jpg', 'jpeg', 'JPG', 'JPEG', 'webp']
 DEEPFACE_VECTOR_LENGTH = 2622
+
+
+TEST_URLS = ['https://www.hollywoodreporter.com/wp-content/uploads/2022/10/GettyImages-1398676954-H-2022.jpg?w=1296', 
+             'https://parade.com/.image/t_share/MTk3NDgxMjQzNjIzODI2NDM4/kanye-west-wife-kanye-west-bianca-censori.jpg',
+             'https://media.gq-magazine.co.uk/photos/5d13a7d52881ccbf300a9586/16:9/pass/kanye-west-03-gq-7feb19_getty_b.jpg',
+             'https://images0.persgroep.net/rcs/01FOn6gfdTOChv-rnFORx_DpEJM/diocontent/160165041/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8',
+             'https://upload.wikimedia.org/wikipedia/commons/b/bb/Kanye_West_at_the_Met_Gala_in_2019_2.png',
+             'https://ca-times.brightspotcdn.com/dims4/default/6f80eaa/2147483647/strip/false/crop/2514x1556+0+0/resize/1486x920!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F64%2F2d%2Fb889abb448c695d9afcd1afc378e%2Fkim-kardashian-kanye-west-divorce-71287.jpg',
+             'https://assets.euromoneydigital.com/dims4/default/53d1382/2147483647/strip/true/crop/620x380+0+0/resize/840x515!/quality/90/?url=http%3A%2F%2Feuromoney-brightspot.s3.amazonaws.com%2F06%2F30%2Faf0e9e02463a8e05ab6ece2153e1%2Fkanye-west-adidas-logo-2023.png',
+             'https://wwd.com/wp-content/uploads/2019/11/kanye-west-at-fast-company-panel-nyc.jpg?w=1000&h=563&crop=1',
+             'https://images.theconversation.com/files/110255/original/image-20160204-5857-3nvdk7.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop',
+             'https://failurebeforesuccess.com/wp-content/uploads/2022/07/1099403-scaled.jpg']
+
 
 # Checks if file is legal (supported image type).
 def accepted_file(path):
@@ -89,6 +105,20 @@ def faces2matrix(faces):
     return m
 
 
+# Gets and saves image from url. Returns 1 on failure, 0 otherwise.
+def get_image(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("Could not get image:", url)
+        return 1
+    im = Image.open(BytesIO(r.content))
+
+    # Need to find proper suffix for each file
+    im.save('image.jpg')
+
+    return 0
+
+
 if  __name__ == '__main__':
     # Argparser
     parser = argparse.ArgumentParser()
@@ -120,6 +150,8 @@ if  __name__ == '__main__':
         print("No target face found, exiting.")
         exit()
     target_bucket = array2string(get_buckets(target_face))
+    
+    get_image(TEST_URLS[0])
 
 
 
