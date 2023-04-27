c5b18a958f6469832384c0d8c7bf1bdd42b39459
Pepijn <pvwpvw@protonmail.com>
2023-04-27 12:16:23 +0200

    start on face detection

--- main.py
+++ main.py
@@ -6,6 +6,48 @@ from os import listdir
 import os.path
 
 
+ACCEPTED_FORMATS = ['png', 'PNG', 'jpg', 'jpeg', 'JPG', 'JPEG', 'webp']
+
+
+# Checks if file is legal (supported image type).
+def accepted_file(path):
+    if not os.path.isfile(path):
+        return False
+    try:
+        suffix = path.split('.')[-1]
+    except Exception as e:
+        return False
+    if suffix in ACCEPTED_FORMATS:
+        return True
+    else:
+        return False
+
+
+# Finds and returns all face information of faces in an image.
+def find_faces(path):
+    if not accepted_file(path):
+        return None
+    return DeepFace.represent(img_path = path)
+
+
+
+
+
+# Finds target face
+def find_target(path):
+    faces = find_faces(path)
+    if faces is None:
+        return None
+    largest_size = 0
+    for face in faces:
+        v = face['embedding']
+        fa = face['facial_area']
+        if fa['w'] > largest_size:
+            size = fa['w']
+            best_fit = v
+
+    return np.array(best_fit)
+
 
 
 if __name__ == '__main__':
